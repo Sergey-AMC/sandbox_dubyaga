604e17d57375604f986595f8add216d5a17753c4
Sergey-AMC <sdubyaga1978@gmail.com>
2026-07-04 01:09:55 +0300

    Add test to check Title on the Main Page

---
+++ constants/urls.ts
@@ -0,0 +1,9 @@
+export const URLS = {
+  HOME:         'https://demoqa.com',
+  ELEMENTS:     'https://demoqa.com/elements',
+  FORMS:        'https://demoqa.com/forms',
+  ALERTS:       'https://demoqa.com/alertsWindows',
+  WIDGETS:      'https://demoqa.com/widgets',
+  INTERACTIONS: 'https://demoqa.com/interaction',
+  BOOK:         'https://demoqa.com/books',
+} as const;
